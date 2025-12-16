afa9acf545dedb138bd4bc401b123b7884dd8213
Truong Chi Kien <kientv@MacBook-Pro-cua-Truong.local>
2025-12-16 09:00:03 +0700

    1st commit init

---
+++ src/test/java/com/kientv84/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kientv84.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
